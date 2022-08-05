7114c753d2c49f0239664c70bc83f35a85aff3f2
Gutorov <Folko85@yandex.ru>
2022-08-05 23:13:53 +0300

    Initial commit.

---
+++ src/main/java/ru/gpbi/requestprocessing/RequestProcessingApplication.java
@@ -0,0 +1,13 @@
+package ru.gpbi.requestprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestProcessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestProcessingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/gpbi/requestprocessing/RequestProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gpbi.requestprocessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
